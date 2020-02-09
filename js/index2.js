a59532dca66f5be7623ba7d82ab556b0f97483dc
Anatole HAGBE <ahagbes89@gmail.com>
2020-02-09 01:41:27 +0100

    First commit

---
+++ js/index2.js
@@ -0,0 +1,60 @@
+let mymap = L.map("mymap"), present={length:0}, flag = 1, firstTime =true;
+let cluster = L.markerClusterGroup();
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+	maxZoom: 5,
+	minZoom: 1,
+	attribution: '<a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(mymap);
+mymap.addLayer(cluster);
+
+async function loadData(url){
+	let latlngs=[];
+	let markers = await fetch(url).then(response=>{
+		return response.json();
+	});
+	markers = (markers.length===undefined)?[markers]:markers;
+	markers.forEach(marker =>{
+	if(firstTime){
+		latlngs.push([marker.latitude, marker.longitude]);
+	}
+		update(marker);
+	});
+	clear();
+	if(firstTime){
+		mymap.fitBounds(latlngs);
+		firstTime = false;
+	}
+}
+
+
+function update(data){
+	let coord = [data.latitude, data.longitude], id = data.id, marker;
+	let text = "<strong>"+data.name+"</strong><br/>("+coord[0]+" ,"+coord[1]+")";
+	if(present[id]===undefined){
+		marker = L.marker(coord);
+		cluster.addLayer(marker);
+		present[id]=[flag, marker];
+		present.length +=1;
+	}else{
+		present[id][0] = flag;
+		present[id][1].setLatLng(coord).bindPopup(text);
+	}
+}
+
+function clear(){
+	for(let id in present){
+		if(id!=="length" && present[id][0]!=flag){
+			present[id][1].remove();
+			delete present[id];
+			present.length -=1;
+		}
+	}
+	flag = 1-flag;
+}
+
+//let url = "https://api.wheretheiss.at/v1/satellites/25544";
+let url = "./base/data.json";
+setInterval(function(){
+	loadData(url);
+},1000);
